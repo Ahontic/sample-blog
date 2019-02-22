93f4b232a386e39c9af4a6f140114be553036161
Quix <nswells14@gmail.com>
2019-02-22 09:41:23 +0200

    Homework contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'contacts/index'
   get '/' => 'home#index'
   resources :articles
+  resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_111138) do
+ActiveRecord::Schema.define(version: 2019_02_22_070113) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.text "name"
+    t.integer "phone"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
