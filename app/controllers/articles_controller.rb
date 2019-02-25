8a8799965e631bd22b07204e2ee3f5177fcb7d81
Quix <nswells14@gmail.com>
2019-02-25 17:20:37 +0200

    Implemented articles#edit articles#update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,36 @@
 class ArticlesController < ApplicationController
+  
   def index
     @articles = Article.all
+  end
+
+   def show
+      @article = Article.find(params[:id])
   end
 
   def new
   end
 
-  def edit
-  end
-
-  def show
-      @article = Article.find(params[:id])
-  end
-
   def create
       # render plain: article_params[:article].inspect
       @article = Article.new(article_params)
-      if @article.valid?
-        @article.save
+      if @article.save
+        redirect_to @article
       else
         render action: 'new'
+      end
+  end
+
+  def edit
+      @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+        redirect_to @article
+      else
+        render action: 'edit'
       end
   end
 
