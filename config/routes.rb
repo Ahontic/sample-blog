9a720e52e2eea2601531c21cfe988a9be433f2f0
Quix <nswells14@gmail.com>
2019-02-27 00:59:25 +0200

    Added comments to post

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
   get '/' => 'home#index'
 
   
-  resources :articles
   resource :contacts, only: [:new,:create], path_names: { :new => ''}
   resources :terms, only: [:index]
   # get 'terms' => 'pages#terms' one controller pages and static page terms
   resources :about, only: [:index]
   # get 'about' => 'pages#about' one controller pages and static page about
-  
+
+resources :articles do
+  resources :comments
+end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
