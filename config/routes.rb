f3b8333e1cfb6b8e2cf04b5041483ef507c2a138
Quix <nswells14@gmail.com>
2019-02-27 00:30:14 +0200

    Added one-to-many article-comments

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # get 'terms' => 'pages#terms' one controller pages and static page terms
   resources :about, only: [:index]
   # get 'about' => 'pages#about' one controller pages and static page about
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
