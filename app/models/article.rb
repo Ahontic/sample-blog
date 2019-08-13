839b9f3344aef8075b8e696087d2a4e8beb6e415
Quix <nswells14@gmail.com>
2019-08-13 11:40:53 +0300

    Added subject method for Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :text, presence: true
   has_many :comments, dependent: :delete_all
+
+  def subject
+    title
+  end
 end
